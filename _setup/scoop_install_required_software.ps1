6d07a629e9cfe105a3efd169c185a065b9ee6040
Jesper Eriksen <eriksenjesp@gmail.com>
2023-12-05 13:20:25 +0100

    add files...

---
+++ _setup/scoop_install_required_software.ps1
@@ -0,0 +1,21 @@
+## Install required software for research assistants using scoop 
+## - The file assumes you have scoop installed 
+
+# Uncomment to setup scoop if not already installed
+# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
+# irm get.scoop.sh | iex
+
+# add scoop bucket "extras"
+scoop bucket add extras
+Scoop bucket add r-bucket https://github.com/cderv/r-bucket.git
+
+# install required software 
+scoop install git
+scoop install vscode
+scoop install r 
+scoop install rstudio
+scoop install rtools
+scoop install tinytex
+scoop install pandoc
+scoop install quarto
+scoop install zotero
